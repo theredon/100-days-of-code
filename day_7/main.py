599d2e2df6ed82a5ca31a477e5a3db33270a5a4e
Theredon <theredon@gmail.com>
2024-02-11 16:27:54 -0700

    Clean up code and revert some solution changes

--- day_7/main.py
+++ day_7/main.py
@@ -1,18 +1,17 @@
-import 
-
+import random
 from hangman_words import word_list
-chosen_word = random.choice(word_list)
-
 from hangman_art import logo, stages
-print(logo)
-# added for testing
-print(f"The solution is {chosen_word}")
 
-display = []
+print(logo)
+
+chosen_word = random.choice(word_list).lower()
 end_of_game = False
 word_length = len(chosen_word)
 lives = 6
+user_guesses = []
 
+# Creates the display.
+display = []
 for _ in range(word_length):
     display.append("_")
 
@@ -20,24 +19,24 @@ print(stages[lives])
 while not end_of_game:
     guess = input("Guess a letter: ").lower()
 
-    if guess in display:
-        print(f"You've already guessed {guess}")
-    
-    for position in range(word_length):
-        if chosen_word[position] == guess:
-            display[position] = chosen_word[position]
+    while guess in user_guesses:
+        guess = input(f"You have already guessed the letter {guess} before, try another: ")
+    user_guesses.append(guess)
 
     if guess not in chosen_word:
         print(f"The letter {guess} is not in the word. You lose a life.")
         lives -= 1
-        if lives == 0:
-            end_of_game = True
-            print("You Lose")
+    else:
+        for position in range(word_length):
+            if chosen_word[position] == guess:
+                display[position] = chosen_word[position]
 
     print(f"{' '.join(display)}")
+    print(stages[lives])
 
     if "_" not in display:
         end_of_game = True
-        print("You win!")
-    
-    print(stages[lives])
+        print("Congratulations!! You win!!")
+    if lives == 0:
+        end_of_game = True
+        print(f"You lose.  The word was {chosen_word}")
