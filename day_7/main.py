28728c8ff0016f812e97f8560843d83ce7e4ee8b
Theredon <theredon@gmail.com>
2024-02-11 15:30:18 -0700

    Add code for Challenge 4

--- day_7/main.py
+++ day_7/main.py
@@ -1,5 +1,62 @@
 import random
 
+stages = ['''
+  +---+
+  |   |
+  O   |
+ /|\  |
+ / \  |
+      |
+=========
+''', '''
+  +---+
+  |   |
+  O   |
+ /|\  |
+ /    |
+      |
+=========
+''', '''
+  +---+
+  |   |
+  O   |
+ /|\  |
+      |
+      |
+=========
+''', '''
+  +---+
+  |   |
+  O   |
+ /|   |
+      |
+      |
+=========
+''', '''
+  +---+
+  |   |
+  O   |
+  |   |
+      |
+      |
+=========
+''', '''
+  +---+
+  |   |
+  O   |
+      |
+      |
+      |
+=========
+''', '''
+  +---+
+  |   |
+      |
+      |
+      |
+      |
+=========
+''']
 word_list = ["ardvark", "baboon", "camel"]
 chosen_word = random.choice(word_list)
 
@@ -9,20 +66,28 @@ print(f"The solution is {chosen_word}")
 display = []
 end_of_game = False
 word_length = len(chosen_word)
+lives = 6
 
 for _ in range(word_length):
     display.append("_")
 
+print(stages[lives])
 while not end_of_game:
     guess = input("Guess a letter: ").lower()
 
-    for position in range(word_length):
-        if chosen_word[position] == guess:
-            display[position] = chosen_word[position]
-    
+    if guess in chosen_word:
+        for position in range(word_length):
+            if chosen_word[position] == guess:
+                display[position] = chosen_word[position]
+    else:
+        lives -= 1
+
+    print(stages[lives])
     print(display)
-    
+
     if "_" not in display:
         end_of_game = True
-
-print("You win!")
+        print("You win!")
+    if lives == 0:
+        end_of_game = True
+        print("You Lose")
