06d4daa27815924eff8f0f8016103cde6fe88f42
Theredon <theredon@gmail.com>
2024-02-11 15:02:54 -0700

    Add code for CHallenge 2 video

--- day_7/main.py
+++ day_7/main.py
@@ -3,10 +3,20 @@ import random
 word_list = ["ardvark", "baboon", "camel"]
 chosen_word = random.choice(word_list)
 
+# added for testing
+print(f"The solution is {chosen_word}")
+
+display = []
+position = 0
+
+for i in range(len(chosen_word)):
+    display.append("_")
+
 guess = input("Guess a letter: ").lower()
 
 for letter in chosen_word:
     if letter == guess:
-        print("Right")
-    else:
-        print("Wrong")
+        display[position] = letter
+    position += 1
+    
+print(display)
