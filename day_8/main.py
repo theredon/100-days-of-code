25e1cc538de87d02999224958cb5df52e144af49
Theredon <theredon@gmail.com>
2024-02-13 20:39:30 -0700

    Encrypting text in caeser cypher

---
+++ day_8/main.py
@@ -0,0 +1,16 @@
+alphabet = ['a', 'b', 'c', 'd', 'e', 'f', 'g', 'h', 'i', 'j', 'k', 'l', 'm', 'n', 'o', 'p', 'q', 'r', 's', 't', 'u', 'v', 'w', 'x', 'y', 'z', ' ']
+
+direction = input("Type 'encode' to encrypt, type 'decode' to decrypt:\n")
+text = input("Type your message: \n").lower()
+shift = int(input("Type the shift number:\n"))
+
+def encrypt(plain_text, cipher_shift):
+    cipher_text = ""
+    for char in plain_text:
+        new_index = alphabet.index(char) + cipher_shift
+        if new_index >= len(alphabet):
+            new_index -= len(alphabet)
+        cipher_text += alphabet[new_index]
+    print(f"The encoded text is {cipher_text}")
+
+encrypt(plain_text=text, cipher_shift=shift)
