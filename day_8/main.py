d3cfa98842a4ff7b4c775c8f7f45d2e3f8c5af39
Theredon <theredon@gmail.com>
2024-02-20 19:28:39 -0700

    Add art, cleanup bugs, add loop for program coninuation until user is done.

--- day_8/main.py
+++ day_8/main.py
@@ -1,14 +1,16 @@
-alphabet = ['a', 'b', 'c', 'd', 'e', 'f', 'g', 'h', 'i', 'j', 'k', 'l', 'm', 'n', 'o', 'p', 'q', 'r', 's', 't', 'u', 'v', 'w', 'x', 'y', 'z', ' ']
+from art import logo
 
-direction = input("Type 'encode' to encrypt, type 'decode' to decrypt:\n")
-text = input("Type your message: \n").lower()
-shift = int(input("Type the shift number:\n"))
+alphabet = ['a', 'b', 'c', 'd', 'e', 'f', 'g', 'h', 'i', 'j', 'k', 'l', 'm', 'n', 'o', 'p', 'q', 'r', 's', 't', 'u', 'v', 'w', 'x', 'y', 'z', ' ']
+should_continue = "yes"
 
 def caeser(plain_text, cipher_shift, shift_direction):
     cipher_text = ""
     if shift_direction == 'decode':
         cipher_shift *= -1
     for char in plain_text:
+        if char not in alphabet:
+            cipher_text += char
+            continue
         new_index = alphabet.index(char) + cipher_shift
         if shift_direction == "decode" and new_index < 0:
             new_index += len(alphabet)
@@ -18,5 +20,17 @@ def caeser(plain_text, cipher_shift, shift_direction):
     
     print(f"The {shift_direction}d text is {cipher_text}")
     
+print(logo)
 
-caeser(plain_text=text, cipher_shift=shift, shift_direction=direction)
+while should_continue == "yes":
+    direction = input("Type 'encode' to encrypt, type 'decode' to decrypt: ")
+    text = input("Type your message: ").lower()
+    shift = int(input("Type the shift number: "))
+    
+    shift = shift % len(alphabet)
+
+    caeser(plain_text=text, cipher_shift=shift, shift_direction=direction)
+
+    should_continue = (input("Would you like to encode/decode another message? yes or no? "))
+
+print("Goodbye.")
