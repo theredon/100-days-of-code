f15e583b1d6c47d11b7804f03b8a289599c2fd8a
Theredon <theredon@gmail.com>
2024-02-20 19:47:15 -0700

    Interactive Coding Excercise - Grading program

---
+++ day_9/grading_excercise.py
@@ -0,0 +1,23 @@
+student_scores = {
+    "Harry": 81,
+    "Ron": 78,
+    "Hermione": 99,
+    "Draco": 74,
+    "Neville": 62,
+}
+
+# TODO-1: Create an empty dictionary called student_grades.
+student_grades = {}
+
+# TODO-2: Write your code below to add the grades to the studen_grades dictionary:
+for key in student_scores:
+    if student_scores[key] >= 91 and student_scores[key] <= 100:
+        student_grades[key] = "Outstanding"
+    elif student_scores[key] >= 81 and student_scores[key] <= 90:
+        student_grades[key] = "Exceeds Expectations"
+    elif student_scores[key] >= 71 and student_scores[key] <= 80:
+        student_grades[key] = "Acceptable"
+    elif student_scores[key] <= 70:
+        student_grades[key] = "Fail"
+
+print(student_grades)
