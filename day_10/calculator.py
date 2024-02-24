66c428a983b17d59a44b36c65933ccb6bd366064
Theredon <theredon@gmail.com>
2024-02-24 15:26:39 -0700

    Added recursion to continue calculating and a way to end

--- day_10/calculator.py
+++ day_10/calculator.py
@@ -15,24 +15,33 @@ def multiply(n1, n2):
 def divide(n1, n2):
     return n1 / n2
 
+
+def calculator():
+    calculator_continue = True
+    first_num = int(input("What is the first number?: "))
+    for key in operations:
+        print(key)
+    while calculator_continue:
+        operation_choice = input("Pick an operation: ")
+        second_num = int(input("What is the next number?: "))
+        function = operations[operation_choice]
+        result = function(first_num, second_num)
+        print(f"{first_num} {operation_choice} {second_num} = {result}")
+        program_continue = input(f"Type 'y' to continue calculating with {result}, type 'n' to start a new calculation, or type 'end' to end.: ")
+        if program_continue == 'y':
+            first_num = result
+        elif program_continue == 'n': 
+            calculator_continue = False
+            calculator()
+        else:
+            calculator_continue = False
+
+
 operations = {"+": add,
               "-": subtract,
               "*": multiply,
               "/": divide}
-calculator_continue = True
 
 print(logo)
 
-first_num = int(input("What is the first number?: "))
-for key in operations:
-    print(key)
-while calculator_continue:
-    operation_choice = input("Pick an operation: ")
-    second_num = int(input("What is the next number?: "))
-    function = operations[operation_choice]
-    result = function(first_num, second_num)
-    print(f"{first_num} {operation_choice} {second_num} = {result}")
-    if input(f"Type 'y' to continue calculating with {result}, or type 'n' to exit.:") == 'y':
-        first_num = result
-    else: 
-        calculator_continue = False
+calculator()
