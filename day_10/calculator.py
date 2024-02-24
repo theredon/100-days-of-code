9a58f335058c86bd1fac5af7ba7395368b0e3591
Theredon <theredon@gmail.com>
2024-02-24 15:17:08 -0700

    Tweaked while loop to match instructor's

--- day_10/calculator.py
+++ day_10/calculator.py
@@ -19,19 +19,20 @@ operations = {"+": add,
               "-": subtract,
               "*": multiply,
               "/": divide}
-calculator_continue = 'y'
+calculator_continue = True
 
 print(logo)
 
 first_num = int(input("What is the first number?: "))
 for key in operations:
     print(key)
-while calculator_continue == 'y':
+while calculator_continue:
     operation_choice = input("Pick an operation: ")
     second_num = int(input("What is the next number?: "))
     function = operations[operation_choice]
     result = function(first_num, second_num)
     print(f"{first_num} {operation_choice} {second_num} = {result}")
-    calculator_continue = input(f"Type 'y' to continue calculating with {result}, or type 'n' to exit.:")
-    if calculator_continue == 'y':
+    if input(f"Type 'y' to continue calculating with {result}, or type 'n' to exit.:") == 'y':
         first_num = result
+    else: 
+        calculator_continue = False
